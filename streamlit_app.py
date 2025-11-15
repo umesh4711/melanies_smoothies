37b5624f962c02b946b4bd2f859aee6ce6be74c2
umesh4711 <167001155+umesh4711@users.noreply.github.com>
2025-11-15 20:21:04 +0530

    Display Smoothiefroot nutrition information
    added/updated below
    st.text(smoothiefroot_response.json())

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
+#new section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
